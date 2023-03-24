47382ee636af32e0ef4515c7b8e63d44529ad982
Rik Bose <88793134+infiniterik@users.noreply.github.com>
2023-03-24 03:52:55 +0000

    Refactor file names for better example

--- runtest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.fail;
-import org.junit.Test;
-
-public class runtest {
-    @Test
-    public void test() {
-        assert test.foo() == false;
-    }
-}
--- src/Foo.java
+++ src/Foo.java
@@ -1,4 +1,4 @@
-public class test {
+public class Foo {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
